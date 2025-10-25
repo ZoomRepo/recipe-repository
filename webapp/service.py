afda6a42e13361699c79631a6251fca5d8950c02
Ollie Beumkes <ollybeumkes@hotmail.co.uk>
2025-10-25 18:21:56 +0100

    Add dynamic cuisine filters from recipe metadata

--- webapp/service.py
+++ webapp/service.py
@@ -1,15 +1,19 @@
 """Application services for the recipe web interface."""
 from __future__ import annotations
 
-from typing import Iterable, Optional, Sequence
+from typing import Iterable, Mapping, Optional, Sequence, Tuple
 
-from .models import PaginatedResult, RecipeDetail
 from .filter_options import (
-    CUISINE_LOOKUP,
+    CUISINE_OPTIONS,
     DIET_LOOKUP,
     MEAL_LOOKUP,
+    FilterOption,
+    build_dynamic_cuisine_options,
+    build_lookup,
+    merge_options,
     normalize_selection,
 )
+from .models import PaginatedResult, RecipeDetail
 from .repository import RecipeQueryRepository
 
 
@@ -19,6 +23,8 @@ class RecipeService:
     def __init__(self, repository: RecipeQueryRepository, page_size: int) -> None:
         self._repository = repository
         self._page_size = page_size
+        self._cuisine_options: Optional[Tuple[FilterOption, ...]] = None
+        self._cuisine_lookup: Optional[Mapping[str, FilterOption]] = None
 
     def search(
         self,
@@ -46,7 +52,13 @@ class RecipeService:
                 normalized_ingredients.append(cleaned)
                 seen.add(cleaned)
 
-        normalized_cuisines = normalize_selection(cuisines or [], CUISINE_LOOKUP)
+        cuisine_lookup = self.cuisine_lookup()
+        normalized_cuisines = normalize_selection(cuisines or [], cuisine_lookup)
+        if cuisines and len(normalized_cuisines) < len(cuisines):
+            # New cuisines may have been added since the options were loaded
+            self.refresh_cuisine_filters()
+            cuisine_lookup = self.cuisine_lookup()
+            normalized_cuisines = normalize_selection(cuisines or [], cuisine_lookup)
         normalized_meals = normalize_selection(meals or [], MEAL_LOOKUP)
         normalized_diets = normalize_selection(diets or [], DIET_LOOKUP)
 
@@ -58,7 +70,32 @@ class RecipeService:
             normalized_cuisines,
             normalized_meals,
             normalized_diets,
+            cuisine_lookup=cuisine_lookup,
         )
 
     def get(self, recipe_id: int) -> Optional[RecipeDetail]:
         return self._repository.get(recipe_id)
+
+    def cuisine_options(self) -> Tuple[FilterOption, ...]:
+        self._ensure_cuisine_filters()
+        assert self._cuisine_options is not None
+        return self._cuisine_options
+
+    def cuisine_lookup(self) -> Mapping[str, FilterOption]:
+        self._ensure_cuisine_filters()
+        assert self._cuisine_lookup is not None
+        return self._cuisine_lookup
+
+    def refresh_cuisine_filters(self) -> None:
+        self._cuisine_options = None
+        self._cuisine_lookup = None
+        self._ensure_cuisine_filters()
+
+    def _ensure_cuisine_filters(self) -> None:
+        if self._cuisine_options is not None and self._cuisine_lookup is not None:
+            return
+        dynamic_labels = self._repository.list_cuisine_labels()
+        dynamic_options = build_dynamic_cuisine_options(dynamic_labels, CUISINE_OPTIONS)
+        combined = merge_options(CUISINE_OPTIONS, tuple(dynamic_options))
+        self._cuisine_options = combined
+        self._cuisine_lookup = build_lookup(combined)
